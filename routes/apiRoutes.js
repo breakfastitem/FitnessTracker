010d4f1e52e19d62c22927319a6cb31c8062365f
breakfastitem <andrew.ehrman@gmail.com>
2021-02-23 17:41:23 -0500

    Added post workout route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,28 @@
 const mongoose = require("mongoose");
 
 module.exports = function (app, db) {
+    //Put Routes
     app.put("/api/workouts/:workoutId", (req, res) => {
 
         db.Workout.findOneAndUpdate({ _id: req.params.workoutId }, { $push: { exercises: req.body } }, (err, data) => {
             if (err) throw err;
-            console.log(data);
+            
             res.json(data);
         });
     });
 
-
+    //Post routes
     app.post("/api/workouts", ({ body }, res) => {
-        console.log(body);
-        db.Workout.create(body);
+        db.Workout.create(body,(err,workout)=>{
+            if(err) throw err;
 
-        res.json(200);
+            res.json(workout);
+        });
+
+       
     });
 
+    //Get routes
     app.get("/api/workouts", (req, res) => {
         db.Workout.find({}, function (err, data) {
             if (err) throw err;
@@ -25,7 +30,6 @@ module.exports = function (app, db) {
             res.json(data);
         })
     });
-
 
 
     app.get("/api/workouts/range", (req, res) => {
