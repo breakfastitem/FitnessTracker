4caeaf39d4e37d02e4218afe9861fdae459fc790
breakfastitem <andrew.ehrman@gmail.com>
2021-02-23 16:26:56 -0500

    Added morgan to track api responses

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
@@ -7,6 +9,7 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 //middleware
+app.use(morgan("tiny"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
